4913db8ba76e57e17385e8f433c8f978fdf03c4a
sheriff legrand <sheriff@sheriffs-Air.lan>
2024-03-25 17:54:48 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let inputValue = 10;
+
+milesToKm = true;
+
+let result = 0;
+
+if (milesToKm) {
+  result = inputValue * 1.60934;
+} else {
+  result = inputValue / 1.60934;
+}
+
+const resultString = inputValue + " miles are " + result + " km";
+
+console.log(resultString);
+
+const resultElement = document.getElementById("resultElement");
+
+resultElement.innerHTML= resultString;
